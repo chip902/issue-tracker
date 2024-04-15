80b46c0461e1944932c22473ae3cf1e7c8be54cc
chip902 <68761150+chip902@users.noreply.github.com>
2024-04-14 23:43:32 -0400

    Improved Loading Experience

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,5 +1,8 @@
+import dynamic from "next/dynamic";
 import React from "react";
-import IssueForm from "../_components/IssueForm";
+import IssueFormSkeleton from "./loading";
+
+const IssueForm = dynamic(() => import("@/app/issues/_components/IssueForm"), { ssr: false, loading: () => <IssueFormSkeleton /> });
 
 const NewIssuePage = () => {
 	return <IssueForm />;
