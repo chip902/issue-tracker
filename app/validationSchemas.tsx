d3e49cb29249a2dc3fe14985a585ceea070e8615
chip902 <68761150+chip902@users.noreply.github.com>
2024-05-18 00:18:43 -0400

    expand PATCH Issue APIx

--- app/validationSchemas.tsx
+++ app/validationSchemas.tsx
@@ -2,5 +2,11 @@ import { z } from "zod";
 
 export const issueSchema = z.object({
 	title: z.string().min(1, "Please enter a valid title.").max(255),
-	description: z.string().min(1, "Your description is too short."),
+	description: z.string().min(1, "Your description is too short.").max(65535),
+});
+
+export const patchIssueSchema = z.object({
+	title: z.string().min(1, "Please enter a valid title.").max(255).optional(),
+	description: z.string().min(1, "Your description is too short.").max(65535).optional(),
+	assignedToUserId: z.string().min(1, "Assign to user id is required.").max(255).optional().nullable(),
 });
